b060b2ea30908640f3dc7774b5db43442d30ca25
Espanholeitor <gomezruizandres@gmail.com>
2025-03-18 17:39:55 +0100

    Last commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+from play_procedures import *
 
 root = tk.Tk()
-root.title("Poker Preflop Strategy")
-root.geometry("500x200")
+root.title("Poker Strategy")
+root.geometry("550x600")
 root.resizable(False, False)
-
 
 
 # Hand Chooser Widget
@@ -15,7 +15,8 @@ tk.Label(
 ).grid(row=0, column=0, padx=0, pady=0, sticky=tk.W)
 Hand = ttk.Combobox(
     root,
-    values=["AA or KK", "AK", "QQ", "JJ to 22"],
+    values=["Big Pairs (AA/KK)", "Ace King AK", "Queens QQ", "Small Pairs (JJ to 22)", "Small Connecting Cards",
+            "Borderline Hands", "Trash Hands"],
     state="readonly",
 )
 Hand.grid(row=1, column=0, padx=0, pady=0, sticky=tk.W)
@@ -29,22 +30,41 @@ submit = ttk.Button(
 submit.grid(row=1, column=0, padx=145, pady=0, sticky=tk.W)
 
 # Text with Hand sugested game Widget
-T = tk.Text(root, height = 9, width = 61)
+T = tk.Text(root, height = 34, width = 64)
 T.grid(row=3, column=0, columnspan=2, padx=0, pady=0, sticky=tk.W)
 
 
 # Set the results
 def Take_input():
-    if Hand.get() == "AA or KK":
-       Tip = "Raise"
+    if Hand.get() == "Big Pairs (AA/KK)":
+       Tip = Big_Pairs
        T.delete('1.0', tk.END)
        T.insert(tk.END, Tip)
-    elif Hand.get() == "JJ to 22":
-       Tip = "Fold"
+    elif Hand.get() == "Ace King AK":
+       Tip = AK
        T.delete('1.0', tk.END)
        T.insert(tk.END, Tip)
-
-
+    elif Hand.get() == "Queens QQ":
+       Tip = QQ
+       T.delete('1.0', tk.END)
+       T.insert(tk.END, Tip)
+    elif Hand.get() == "Small Pairs (JJ to 22)":
+       Tip = JJ_to_22
+       T.delete('1.0', tk.END)
+       T.insert(tk.END, Tip)
+    elif Hand.get() == "Small Connecting Cards":
+       Tip = SCC
+       T.delete('1.0', tk.END)
+       T.insert(tk.END, Tip)
+    elif Hand.get() == "Borderline Hands":
+       Tip = BH
+       T.delete('1.0', tk.END)
+       T.insert(tk.END, Tip)
+    elif Hand.get() == "Trash Hands":
+       Tip = TH
+       T.delete('1.0', tk.END)
+       T.insert(tk.END, Tip)
+       
 
 
 
---
+++ play_procedures.py
@@ -0,0 +1,169 @@
+Big_Pairs = """[PREFLOP]
+*In an early position just call the blind hoping
+that somebody raise it behing so you could re raise
+
+*In a middle position if nobody came in just call the blind,
+if anybody came in raise the size of the pot
+
+*In a late position raise, if got reraised then go ALL IN
+
+**Rare situation to consider fold KK: You are sure that the
+oponent that has reraised you has AA
+***********************************************************
+[FLOP]
+**Situation: you have KK and there is a single Ace on the Flop,
+if you put your oponent in an Ace, thats the end of the Pot,
+if not you play as you have best Hand
+
+*In early or middle position check if you see Straight or Flush draws,
+play carefully specially if there is two or more people in the pot
+
+*In late make small bet, if you got called play carefully
+
+*If the Flop came 3 hards and you have AhA you play,
+if your AA or KK do not match the flush draw in the Flop
+play carefully or fold
+
+*If the Flop is a pair with a rag (with Straight or Flush draw, 662) bet,
+if got called then play carefully
+
+*If the Flop is a pair with a rag (without Straight or Flush draw,772,77K),
+then you can slow play, you can give free card
+
+*If you turn a set on the Flop (three Aces), do a large bet
+
+*If you tune a set on the Flop (three Kings), you can give a free card if
+no straight flush draw is present
+
+*If you Flop a set of four (4 Kings or 4 Aces), check until someone bets,
+then call and make call/raise at the end if you want
+***********************************************************
+[TURN AND RIVER]
+*make your own judgement (read the full book)
+"""
+
+
+AK = """[PREFLOP]
+*In early position Raise the blind, if you got raised reraise or go ALL In
+*In middle if someone hast raised before just call
+*In late position Raise
+**Remember: AK Suited has more value than AK offsuit.
+**In general go ALL in when you got reraised or when there is
+already several players in and the pot has already a good amount
+***********************************************************
+[FLOP]
+*If you are the original bettor or raiser bet regardless of what comes but
+if the flop did not help you and someone raises just give up. But if something
+like Jh 10h 9h flops do not bluf
+*If you got an A or K in the flop raise good
+*If you flop two As or two Ks check if the third card gives not a draw but only
+if you are last to act
+**You should never check when there is draw, always bet
+**If you got set of fours, check until you can
+**If Q J 10 floped do a superhigh bet if you were the raiser and no so big if
+you were not the raiser
+**Also bet if you flop two flush cards with your AK suited
+***********************************************************
+[TURN AND RIVER]
+*Bet if you think your oponent did not make his hand, check if the oposite
+"""
+
+QQ = """[PREFLOP]
+*Just raise when nobody rised before you
+*If someone raised in  front of you just call
+**Play this hand very careful
+**Only move agrassive if you got a lot of callers in front of you
+**try to get the set
+***********************************************************
+[FLOP]
+*If an A or K came in the Flop give up if someone bet
+*If there is no A or K in the the flop and no flush draw, play
+as if you have AA or KK
+*with flush draw be careful again
+***********************************************************
+[TURN and RIVER]
+*Same as AA and KK while there is not A or K pr flush draw in the table
+"""
+
+JJ_to_22 = """[PREFLOP]
+*Call the blind or call if someone raise from early or middle
+*You can reraise a weak raiser in late position with JJ 1010 or 99 
+*with 88 to 22 Do not raise
+*Give up if you got reraised
+**JJ,1010 and 99 play stroger as 88 to 22
+**Try to get the set
+***********************************************************
+[FLOP]
+*If you was raiser and got over pair bet
+*If you was raiser and got set bet (good bet)
+*If there is a draw bet, if you got raised fold
+*If Flop did not help only bet if you raised before, if you get called
+do not bet again unless you think oponent is in draw
+*If you got reraised fold
+*If you flop a set raise or call, never check
+*If you flop small full make good bets
+*If you flop big full check and give free card or change to reraise
+
+"""
+
+SCC = """[PREFLOP]
+**Suited connecting cards: 7c-6c,8h-7h, 5d-4d
+**Also connected or not but suited except top botton of straight 8c-4c,
+Jh-7h...
+**Also any Ax or Kx suited
+*Call in any position and raise in late
+*Call to a raiser also in the flop
+**In general do not ivolve more than 5-10% of your chips here
+***********************************************************
+[FLOP TURN AND RIVER]
+*If you miss hand completely and you just called before, you give up
+*If you miss and was raiser before, bet. If you got raised then fold
+*If you were raiser and something like AAK floped, bet as others can think
+you got A or K set or full house
+*In the Turn and River *If you have nothing and you got called previously
+you can bet if you think he was drawing and did not make his hand,
+If you think he made his hand check along and fold when the other bet
+-----------------
+--When flop improved your hand
+**If you have 7d-6d and got Qs-6h-2c thats a poor flop,
+play as if you miss the flop so bet if you was raiser
+**If you got a pair in the flop and you bet in the flop and got called
+you could bet again in fourth, if in fith nothin help stop betting.
+**If you got help on fourth (you got two pairs) you can overbet but
+only if you were original raiser
+**If you got help on fourth (you got two pairs)
+and you were just a caller previously you could do a check-raise if the
+guy after you bets
+
+**If you got top pair, straight draw or flush draw you have a fair flop.
+**With Straigh draw bet and or raised all in if you was
+origina raiser. If you did check before the flop, you can do check-raise
+raise all in.
+**Play slow with Top pair or Flush draw
+***If you got top pair and straigh draw, set, straight or flush yo got
+good flop, bet and raise
+"""
+
+
+BH = """
+*AQ, AJ, AT, KQ, KJ, JT, QJ, QT JT, 98 (all non suited) are trouble hands
+*In short games (max 4 players) are good hands
+*Never call a Raise out of position
+*If Raised and you are at late position cosider call but carefull
+*play carefully even when you flop pair, set or draw
+**If they are suited you can pal more agresive, check raises can be
+a good strategy here
+"""
+
+TH = """
+**Except Ax or Kx suited any other hand that is not in previous
+classification is a Trash hand
+**With these hands you dont want to play out of position
+**If you are in late position and you think you will face just
+one oponent you can call, if you do not get a good flop give up
+**If you bet or raised on before the flop you can raise to try to get
+a cheap pot in the flop
+**Again in most situation this hands are not playable
+
+
+"""
